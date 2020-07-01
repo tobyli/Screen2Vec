8c31d4ddc1c7665305cb6b4aac55e816b33bbb56
lipopowski <lpopowski@hmc.edu>
2020-06-30 20:35:02 -0700

    Fixed saving method

--- UI_embedding/write_vocab_embedding.py
+++ UI_embedding/write_vocab_embedding.py
@@ -1,12 +1,12 @@
 from sentence_transformers import SentenceTransformer
 import json
+import numpy as np
 
-with open("vocab.json") as f:
+with open("dataset/vocab.json") as f:
     vocab_list = json.load(f, encoding='utf-8')
 
 bert = SentenceTransformer('bert-base-nli-mean-tokens')
 
 vocab_emb = bert.encode(vocab_list)
 
-with open('emb_vocab.json', 'w', encoding='utf-8') as f:
-    json.dump(vocab_emb, f, indent=4)
+np.save("vocab_emb", vocab_emb)
