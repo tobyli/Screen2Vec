c3d058b499adc41bebfb7f2cf00f7b4eead52f54
lipopowski <lpopowski@hmc.edu>
2020-07-06 15:58:46 -0700

    updated model_tester to print some examples

--- UI_embedding/model_tester.py
+++ UI_embedding/model_tester.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 import torch.nn as nn
 from sentence_transformers import SentenceTransformer
@@ -11,13 +12,23 @@ from prepretrainer import UI2VecTrainer
 from dataset.dataset import RicoDataset, RicoScreen, ScreenDataset
 from dataset.vocab import BertScreenVocab
 
-n = 4
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-m", "--model", required=True, type=str, help="path to pretrained model to test")
+parser.add_argument("-n", "--num_predictors", type=int, default=10, help="number of other labels used to predict one when model was trained")
+parser.add_argument("-r", "--range", type=int, default=10, help="number of other labels used to predict one when model was trained")
+parser.add_argument("-v", "--vocab_path", required=True, type=str, help="path to json of text in vocab")
+parser.add_argument("-x", "--extra", type=int, default=0, help="1 to display some extra results")
+args = parser.parse_args()
+
+n = args.num_predictors
 bert_size = 768
 bert = bert = SentenceTransformer('bert-base-nli-mean-tokens')
 model = HiddenLabelPredictorModel(bert, bert_size, n)
-model.load_state_dict(torch.load('output_model.ep100'))
+model.load_state_dict(torch.load(args.model))
 
-vocab_path = 'dataset/vocab_sm.json'
+vocab_path = args.vocab_path
 
 with open(vocab_path) as f:
     vocab_list = json.load(f, encoding='utf8')
@@ -33,21 +44,27 @@ dataset_rico = RicoDataset(input_path)
 dataset = ScreenDataset(dataset_rico, n)
 data_loader = DataLoader(dataset, batch_size=1)
 
+
+i = 0
 for data in data_loader:
 # run it through the network
     element = data[0]
     context = data[1]
     # forward the training stuff (prediction)
-    prediction_output = model.forward(context) #input here
+    prediction_output = model(context) #input here
     element_target_index = vocab.get_index(element[0])
     # find which vocab vector has the smallest cosine distance
     distances = scipy.spatial.distance.cdist(prediction_output.detach().numpy(), vocab.embeddings, "cosine")
 
-    temp = np.argpartition(-distances, n*10)
-    closest_idx = temp[:(n*10)]
+    temp = np.argpartition(-distances, args.range)
+    closest_idx = temp[:args.range]
     if element_target_index in closest_idx:
         correct +=1
     total+=1
+
+    if (i<100):
+        i+=1
+        print("intended: " + vocab.get_text(element_target_index) + " predicted: " + vocab.get_text(closest_idx[0]))
 # find which vocab vector has the largest dot product
 
 print(correct/total)
