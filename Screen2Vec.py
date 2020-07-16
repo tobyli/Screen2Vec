fixing data leak in validation iterations; partially implemented use of location info-not enabled yet

Screen2Vec.py
@@ -8,16 +8,16 @@ class Screen2Vec(nn.Module):
     Model intended to semantically embed the content of UI screens into vectors
     """
 
-    def __init__(self, bert_size=768, num_classes=24):
+    def __init__(self, bert_size=768, num_classes=24, additional_ui_size =0, additional_size_screen=0):
         """
         describe params here
         """
         super().__init__()
         self.bert_size = bert_size
-        self.net = nn.RNN(bert_size, bert_size)
-        self.lin = nn.Linear(self.bert_size*2, self.bert_size)
+        self.net = nn.RNN(bert_size+additional_ui_size, bert_size)
+        self.lin = nn.Linear(self.bert_size*2+additional_size_screen, self.bert_size)
 
-    def forward(self, UIs, descr, trace_screen_lengths):
+    def forward(self, UIs, descr, trace_screen_lengths, screen_vecs=None):
         """
         UIs: batch_size x screen_size x trace_length x bert_size
         descr: batch_size x trace_length x bert_size
@@ -33,6 +33,8 @@ class Screen2Vec(nn.Module):
             # if this isn't working, come back here and unpack output, then take last of each
             # add on screen description embedding
             concat_emb = torch.cat((h, descr[batch_num]), dim=1)
+            if screen_vecs:
+                concat_emb = torch.cat((concat_emb, screen_vecs[batch_num]))
             # bring down to bert size through a linear layer
             final_emb = self.lin(concat_emb)
             screen_embeddings[batch_num] = final_emb

dataset/dataset.py
@@ -15,11 +15,12 @@ class RicoDataset(Dataset):
     '''
     has traces, which have screens
     '''
-    def __init__(self, num_preds, ui, ui_e, d, d_e, fully_load=True):
+    def __init__(self, num_preds, ui, ui_e, d, d_e, net_version=0, fully_load=True):
         self.traces = []
         self.n = num_preds + 1
         self.ui_e = ui_e
         self.d_e = d_e
+        self.setting = net_version
         if fully_load:
             self.load_all_traces(ui, d)
 
@@ -29,8 +30,12 @@ class RicoDataset(Dataset):
         if len(indexed_trace.trace_screens) >= self.n:
             starting_index = random.randint(0, len(indexed_trace.trace_screens)-self.n)
             screens = indexed_trace.trace_screens[starting_index:starting_index+self.n-1]
-        return [[torch.tensor(screen.UI_embeddings) for screen in screens], [screen.descr_emb for screen in screens], [index, starting_index + self.n - 1]]
-    
+        if self.setting in [0,2]:
+            return [[torch.tensor(screen.UI_embeddings) for screen in screens], [screen.descr_emb for screen in screens], [index, starting_index + self.n - 1]]
+        else:
+            # get the right part of labeled text
+            return [[torch.tensor(screen.UI_embeddings + screen.labeled_text[2]) for screen in screens], [screen.descr_emb for screen in screens], [index, starting_index + self.n - 1]]
+
     def __len__(self):
         return len(self.traces)
 

main_preloaded.py
@@ -37,11 +37,10 @@ parser.add_argument("-o", "--output_path", required=True, type=str, help="where 
 parser.add_argument("-b", "--batch_size", type=int, default=64, help="traces in a batch")
 parser.add_argument("-e", "--epochs", type=int, default=10, help="number of epochs")
 parser.add_argument("-n", "--num_predictors", type=int, default=10, help="number of other labels used to predict one")
-parser.add_argument("-l", "--loss", type=int, default=1, help="1 to use cosine embedding loss, 0 to use softmax dot product")
 parser.add_argument("-r", "--rate", type=float, default=0.001, help="learning rate")
 parser.add_argument("-s", "--neg_samp", type=int, default=128, help="number of negative samples")
 parser.add_argument("-a", "--prev_model", type=str, default=None, help="previously trained model to start training from")
-
+parser.add_argument("-v", "--net-version", type=str, default=0, help="0 for regular, 1 to embed location in UIs, 2 to use layout embedding, and 3 to use both")
 
 
 args = parser.parse_args()
@@ -73,8 +72,8 @@ with open(args.test_data + "descr.json") as f:
 te_descr_emb = np.load(args.test_data + "dsc_emb.npy")
 
 
-train_dataset = RicoDataset(args.num_predictors, tr_uis, tr_ui_emb, tr_descr, tr_descr_emb)
-test_dataset = RicoDataset(args.num_predictors, te_uis, te_ui_emb, te_descr, te_descr_emb)
+train_dataset = RicoDataset(args.num_predictors, tr_uis, tr_ui_emb, tr_descr, tr_descr_emb, args.net_version)
+test_dataset = RicoDataset(args.num_predictors, te_uis, te_ui_emb, te_descr, te_descr_emb, args.net_version)
 
 vocab_train = ScreenVocab(train_dataset)
 vocab_test = ScreenVocab(test_dataset)
@@ -83,14 +82,26 @@ train_data_loader = DataLoader(train_dataset, collate_fn=pad_collate, batch_size
 test_data_loader = DataLoader(test_dataset, collate_fn=pad_collate, batch_size=args.batch_size)
 
 
+#handle different versions of network here
+if args.net_version in [0,2]:
+    adus = 0
+else:
+    # case where coordinates are part of UI rnn
+    adus = 4
+if args.net_version in [0,1]:
+    adss = 0
+else:
+    # case where screen layout vec is used
+    adss = 64
 
-model = Screen2Vec(bert_size)
+model = Screen2Vec(bert_size, num_classes=24, additional_ui_size=adus, additional_size_screen=adss)
 predictor = TracePredictor(model)
+predictor.cuda()
 if args.prev_model:
     predictor.load_state_dict(torch.load(args.prev_model))
 trainer = Screen2VecTrainer(predictor, vocab_train, vocab_test, train_data_loader, test_data_loader, args.rate, args.neg_samp)
 
-predictor.cuda()
+
 test_loss_data = []
 train_loss_data = []
 for epoch in tqdm.tqdm(range(args.epochs)):

prediction.py
@@ -17,4 +17,4 @@ class TracePredictor(nn.Module):
         result = torch.narrow(screens, 1, screens.size()[1]-1, 1).squeeze(1).cuda()
         # run through model
         output, (h,c) = self.combiner(context)
-        return c[0], result
+        return h[0], result