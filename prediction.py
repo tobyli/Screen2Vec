74249c8da440d2d9ca496b666735f5ef4521e3bb
lipopowski <lpopowski@hmc.edu>
2020-07-19 18:00:00 -0700

    option to keep off GPU

--- prediction.py
+++ prediction.py
@@ -9,12 +9,16 @@ class TracePredictor(nn.Module):
         self.bert_size = self.model.bert_size
         self.combiner = nn.LSTM(self.bert_size, self.bert_size, batch_first=True)
 
-    def forward(self, UIs, descr, trace_screen_lengths):
+    def forward(self, UIs, descr, trace_screen_lengths, cuda=True):
         # embed all of the screens
         screens = self.model(UIs, descr, trace_screen_lengths)
         #take all but last element of each trace, store as context
-        context = torch.narrow(screens, 1, 0, screens.size()[1]-1).cuda()
-        result = torch.narrow(screens, 1, screens.size()[1]-1, 1).squeeze(1).cuda()
+        if cuda:
+            context = torch.narrow(screens, 1, 0, screens.size()[1]-1).cuda()
+            result = torch.narrow(screens, 1, screens.size()[1]-1, 1).squeeze(1).cuda()
+        else:
+            context = torch.narrow(screens, 1, 0, screens.size()[1]-1)
+            result = torch.narrow(screens, 1, screens.size()[1]-1, 1).squeeze(1)
         # run through model
         output, (h,c) = self.combiner(context)
         return h[0], result
