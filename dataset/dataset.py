29360f781f90568e5b1586311fe045f40b02b294
lipopowski <lpopowski@hmc.edu>
2020-07-21 15:50:52 -0700

    debug

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -84,6 +84,9 @@ class RicoTrace():
     def load_all_screens(self, ui, d, l, l_idx, s_n):
         print(len(s_n))
         print(len(self.ui_e))
+        if len(s_n) != len(self.ui_e):
+            print(len(ui))
+            print(self.name)
         for screen_idx in range(len(self.ui_e)):
             if s_n:
                 name = s_n[screen_idx]
