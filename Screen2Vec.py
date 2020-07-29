f0e4aa3ef02627036f12afc12ef6fd9f9172f550
lipopowski <lpopowski@hmc.edu>
2020-07-28 20:12:51 -0700

    enable version 4

--- Screen2Vec.py
+++ Screen2Vec.py
@@ -8,9 +8,13 @@ class Screen2Vec(nn.Module):
     Model intended to semantically embed the content of UI screens into vectors
     """
 
-    def __init__(self, bert_size=768, num_classes=24, additional_ui_size =0, additional_size_screen=0, shrink=False):
+    def __init__(self, bert_size=768, additional_ui_size =0, additional_size_screen=0, shrink=False):
         """
-        describe params here
+        -bert_size (int) is the length of a Sentence-BERT text embedding
+        -additional_ui_size and additional_screen_size (int) describe additional lengths for
+        input depending on the network version (whether layout information is part of embedding)
+        -shrink (bool) is True if we intend to transform the description embedding into a lower
+        dimension, False if not
         """
         super().__init__()
         self.bert_size = bert_size
@@ -25,8 +29,10 @@ class Screen2Vec(nn.Module):
 
     def forward(self, UIs, descr, trace_screen_lengths, layouts=None):
         """
-        UIs: batch_size x screen_size x trace_length x bert_size
+        UIs: batch_size x screen_size x trace_length x bert_size + additional_ui_size
         descr: batch_size x trace_length x bert_size
+        trace_screen_lengths: batch_size x trace_length w
+        layouts: None if not used, or batch_size x trace_length x additonal_size_screen
         """
         batch_size = UIs.size()[0]
         screen_embeddings = torch.empty(batch_size, UIs.size()[2], self.bert_size)
@@ -36,8 +42,6 @@ class Screen2Vec(nn.Module):
             input = torch.nn.utils.rnn.pack_padded_sequence(UI_set, trace_screen_lengths[batch_num], enforce_sorted=False)
             full_output, h = self.net(input)
             h = h[0]
-            # if this isn't working, come back here and unpack output, then take last of each
-            # add on screen description embedding
             if self.shrink_desc:
                 descriptions = self.desc_shrinker(descr[batch_num])
             else:
--- prediction.py
+++ prediction.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 from Screen2Vec import Screen2Vec
 
 class TracePredictor(nn.Module):
+    """
+    predicts the embeddings of the next screen in a trace based on its preceding screens
+    """
     def __init__(self, embedding_model: Screen2Vec):
         super().__init__()
         self.model = embedding_model
@@ -12,7 +15,8 @@ class TracePredictor(nn.Module):
     def forward(self, UIs, descr, trace_screen_lengths, layouts=None, cuda=True):
         # embed all of the screens
         screens = self.model(UIs, descr, trace_screen_lengths, layouts)
-        #take all but last element of each trace, store as context
+        # take all but last element of each trace, store as context
+        # last element is the desired result/target
         if cuda:
             context = torch.narrow(screens, 1, 0, screens.size()[1]-1).cuda()
             result = torch.narrow(screens, 1, screens.size()[1]-1, 1).squeeze(1).cuda()
