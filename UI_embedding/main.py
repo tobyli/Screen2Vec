0adb0f1d2bea7d93531ead0855ea8e04712ca88c
lipopowski <lpopowski@hmc.edu>
2020-08-11 07:58:05 -0700

    changed inputs for better test-train divide

--- UI_embedding/main.py
+++ UI_embedding/main.py
@@ -1,7 +1,10 @@
 import argparse
 import json
+import numpy as np
 import tqdm
+import torch
 from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
 from UI2Vec import HiddenLabelPredictorModel 
 from prepretrainer import UI2VecTrainer
 from dataset.dataset import RicoDataset, ScreenDataset
@@ -12,8 +15,9 @@ from plotter import plot_loss
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-c", "--train_dataset", required=True, type=str, help="dataset to train model")
-parser.add_argument("-t", "--test_dataset", required=False, type=str, default=None, help="dataset to test model")
+# parser.add_argument("-c", "--train_dataset", required=True, type=str, help="dataset to train model")
+# parser.add_argument("-t", "--test_dataset", required=False, type=str, default=None, help="dataset to test model")
+parser.add_argument("-d", "--dataset", required=False, type=str, default=None, help="dataset to use to test/train model")
 parser.add_argument("-o", "--output_path", required=True, type=str, help="where to store model")
 parser.add_argument("-b", "--batch_size", type=int, default=64, help="traces in a batch")
 parser.add_argument("-e", "--epochs", type=int, default=10, help="number of epochs")
@@ -33,16 +37,33 @@ with open(args.vocab_path) as f:
 vocab = BertScreenVocab(vocab_list, len(vocab_list), bert, args.embedding_path)
 
 print("Length of vocab is " + str(len(vocab_list)))
-train_dataset_rico = RicoDataset(args.train_dataset)
-train_dataset = ScreenDataset(train_dataset_rico, args.num_predictors)
-train_data_loader = DataLoader(train_dataset, batch_size=args.batch_size)
 
-if args.test_dataset:
-    test_dataset_rico = RicoDataset(args.test_dataset)
-    test_dataset = ScreenDataset(test_dataset_rico, args.num_predictors)
-    test_data_loader = DataLoader(test_dataset, batch_size=args.batch_size, drop_last=True)
-else: 
-    test_data_loader = None
+rico_dataset = RicoDataset(args.dataset)
+dataset = ScreenDataset(rico_dataset, args.num_predictors)
+
+dataset_size = len(dataset)
+indices = list(range(dataset_size))
+split = int(np.floor(0.1 * dataset_size))
+np.random.shuffle(indices)
+train_indices, val_indices = indices[split:], indices[:split]
+
+# Creating PT data samplers and loaders:
+train_sampler = SubsetRandomSampler(train_indices)
+test_sampler = SubsetRandomSampler(val_indices)
+
+train_data_loader = DataLoader(dataset, batch_size=args.batch_size, sampler=train_sampler)
+test_data_loader = DataLoader(dataset, batch_size=args.batch_size, sampler=test_sampler)
+
+# train_dataset_rico = RicoDataset(args.train_dataset)
+# train_dataset = ScreenDataset(train_dataset_rico, args.num_predictors)
+# train_data_loader = DataLoader(train_dataset, batch_size=args.batch_size)
+
+# if args.test_dataset:
+#     test_dataset_rico = RicoDataset(args.test_dataset)
+#     test_dataset = ScreenDataset(test_dataset_rico, args.num_predictors)
+#     test_data_loader = DataLoader(test_dataset, batch_size=args.batch_size, drop_last=True)
+# else: 
+#     test_data_loader = None
 
 predictor = HiddenLabelPredictorModel(bert, 768, args.num_predictors) 
 
