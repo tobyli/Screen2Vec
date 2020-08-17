eb657c3850cb7b0a05b1b7a3d92ae088384c4c60
lipopowski <lpopowski@hmc.edu>
2020-08-17 13:21:49 -0700

    changing version 6

--- prediction.py
+++ prediction.py
@@ -39,8 +39,7 @@ class TracePredictor(nn.Module):
         
         # run screens in trace through model to predict last one
         output, (h,c) = self.combiner(context)
-        if self.net_version in [5,6]:
-            #TODO: work on this part
+        if self.net_version == 5:
             descriptions = torch.narrow(descr, 1, 0, 1)
             h = torch.cat((h[0], descriptions.squeeze(1)), dim=-1)
             result = torch.cat((result,descriptions.squeeze(1)), dim=-1)
