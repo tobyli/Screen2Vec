d88daadb1d904aab3e22474db8d1ec83f33ff4d1
lipopowski <lpopowski@hmc.edu>
2020-08-19 10:38:19 -0700

    changes to do better class assignment

--- UI_embedding/dataset/rico_utils.py
+++ UI_embedding/dataset/rico_utils.py
@@ -13,21 +13,36 @@ def get_all_texts_from_node_tree(node):
                 results.extend(get_all_texts_from_node_tree(child_node))
     return results
 
-def get_all_labeled_texts_from_node_tree(node):
+def get_all_labeled_texts_from_node_tree(node, in_list: bool, in_drawer: bool):
     results = []
     if 'text' in node and isinstance(node['text'], Iterable):
-        text_class = ''
+        text_class = 0
         if node['text'] and node['text'].strip():
             text = node['text']
             if node['class'] and node['class'].strip():
-                text_class = convert_class_to_text_label(node['class'])
+                if node['class'] == 'TextView':
+                    if node['clickable']:
+                        text_class = 20
+                    else:
+                        text_class = 11
+                else:
+                    text_class = convert_class_to_text_label(node['class'])
+            if text_class==0 and (in_drawer or in_list):
+                if in_drawer:
+                    text_class = 25
+                if in_list:
+                    text_class = 24
             if node["bounds"]:
                 bounds = node["bounds"]
             results.append([text, text_class, bounds])
     if 'children' in node and isinstance(node['children'], Iterable):
         for child_node in node['children']:
             if (isinstance(child_node, dict)):
-                results.extend(get_all_labeled_texts_from_node_tree(child_node))
+                if text_class == 12:
+                    in_list = True
+                if text_class == 7:
+                    in_drawer = True
+                results.extend(get_all_labeled_texts_from_node_tree(child_node, in_list, in_drawer))
     return results
 
 def get_all_texts_from_rico_screen(rico_screen: RicoScreen):
@@ -36,6 +51,6 @@ def get_all_texts_from_rico_screen(rico_screen: RicoScreen):
 
 def get_all_labeled_texts_from_rico_screen(rico_screen: RicoScreen):
     if rico_screen.activity is not None and rico_screen.activity.root_node is not None:
-        return get_all_labeled_texts_from_node_tree(rico_screen.activity.root_node)
+        return get_all_labeled_texts_from_node_tree(rico_screen.activity.root_node, False, False)
 
 
