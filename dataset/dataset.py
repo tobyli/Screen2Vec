b811c6cc867d7446d7a6c061605eb34969f806ac
lipopowski <lpopowski@hmc.edu>
2020-09-08 23:30:07 -0700

    debug trace adding

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -121,7 +121,7 @@ class RicoDataset(Dataset):
             self.traces.append(trace_to_add)
         else:
             print("trace length is " + str(len(trace_to_add.trace_screens)))
-            if trace_to_add.trace_screens >= self.n:
+            if len(trace_to_add.trace_screens) >= self.n:
                 print(d)
             
 
