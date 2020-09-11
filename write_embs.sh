a826b1eff3d23dfbfdfef814dd3fd031a02bf087
lipopowski <lpopowski@hmc.edu>
2020-09-10 22:45:20 -0700

    shell script to finish precomp

---
+++ write_embs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python write_trained_models.py -m "output/final_7.ep140" -d "precomp/example" -v 7 -n 4
+python write_trained_models.py -m "output/final_8.ep140" -d "precomp/example" -v 8 -n 4
