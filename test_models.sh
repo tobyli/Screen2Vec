25c2b26b638cb794cb6a6932925e9235c780665a
lipopowski <lpopowski@hmc.edu>
2020-09-11 23:25:28 -0700

    script to run modeltester

---
+++ test_models.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#python modeltester_screen.py -m "output/final_4.ep120" -d "precomp/example" -v 4 -n 4
+#python modeltester_screen.py -m "output/final_6.ep80" -d "precomp/example" -v 6 -n 4
+python modeltester_screen.py -m "output/final_7.ep140" -d "precomp/example" -v 7 -n 4
+python modeltester_screen.py -m "output/final_8.ep140" -d "precomp/example" -v 8 -n 4
