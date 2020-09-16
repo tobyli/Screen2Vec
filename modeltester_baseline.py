e5f740247e665bafb420b3af80ce38dd0f343d9f
lipopowski <lpopowski@hmc.edu>
2020-09-16 14:06:13 -0700

    improved loop working

--- modeltester_baseline.py
+++ modeltester_baseline.py
@@ -18,8 +18,7 @@ class TesterBaselineDataset(Dataset):
         self.n = n
         self.traces = self.load_traces(embeddings)
         self.trace_loc_index = []
-        self.embeddings = []
-        self.load_indices()
+        self.embeddings = self.load_indices()
 
     def __getitem__(self, index):
         indexed_trace = self.traces[index]
@@ -47,10 +46,12 @@ class TesterBaselineDataset(Dataset):
 
     def load_indices(self):
         overall_index = 0
+        embeddings = []
         for trace in self.traces:
-            self.embeddings += trace
+            embeddings += trace
             self.trace_loc_index.append([overall_index + i for i in range(len(trace))])
             overall_index += len(trace)
+        return np.asarray(embeddings)
 
     def get_overall_index(self, trace_idx, screen_idx):
         return self.trace_loc_index[trace_idx][screen_idx]
@@ -62,85 +63,6 @@ parser.add_argument("-l", "--layout_predictor", required=True, type=str, help="p
 parser.add_argument("-v", "--visual_predictor", required=True, type=str, help="path to visual prediction model")
 parser.add_argument("-t", "--text_predictor", required=True, type=str, help="path to text prediction model")
 args = parser.parse_args()
-
-with open(args.precomp + "layout_eval.json") as f:
-    layout_emb = json.load(f, encoding='utf-8')
-
-predictor = BaselinePredictor(len(layout_emb[0][0]))
-predictor.load_state_dict(torch.load(args.layout_predictor))
-
-correct = 0
-toppointzeroone = 0
-toppointone = 0
-topone = 0
-topfive = 0
-total = 0
-
-dataset = TesterBaselineDataset(layout_emb, args.num_predictors)       
-data_loader = DataLoader(dataset, batch_size=1)
-
-data_itr = tqdm.tqdm(enumerate(data_loader),
-                              total=len(data_loader),
-                              bar_format="{l_bar}{r_bar}")
-
-i = 0
-total_se = 0
-total_vector_lengths = 0
-for idx, data in data_itr:
-    trace, index = data
-    trace = trace.squeeze(0)
-    index = index.squeeze(0)
-    result = predictor(trace)
-    i+=len(index)
-    # forward the training stuff (prediction)
-    # find which vocab vector has the smallest cosine distance
-    for idx in range(len(index)):
-        target = index[idx]
-        diff = result[idx].detach()-torch.tensor(dataset.embeddings[target])
-        sqer = sum(diff**2)
-        total_se += sqer
-        total_vector_lengths += np.linalg.norm(diff)
-        distances = scipy.spatial.distance.cdist(result[idx].unsqueeze(0).detach(), dataset.embeddings, "cosine")[0]
-
-        temp = np.argpartition(distances, (0, int(0.0001 * len(distances)), int(0.001 * len(distances)), int(0.01 * len(distances)), int(0.05 * len(distances)), int(0.1 * len(distances))))
-        closest_idx = temp[0]
-        closest_pointzerooneperc = temp[:int(0.0001 * len(distances))]
-        closest_pointoneperc = temp[:int(0.001 * len(distances))]
-        closest_oneperc = temp[:int(0.01 * len(distances))]
-        closest_fiveperc = temp[:int(0.05 * len(distances))]
-
-        if target==closest_idx:
-            correct +=1
-            toppointzeroone+=1
-            toppointone +=1
-            topone +=1
-            topfive +=1
-        elif target in closest_pointzerooneperc:
-            toppointzeroone +=1
-            toppointone +=1
-            topone +=1
-            topfive +=1
-        elif target in closest_pointoneperc:
-            toppointone +=1
-            topone +=1
-            topfive +=1
-        elif target in closest_oneperc:
-            topone +=1
-            topfive +=1
-        elif target in closest_fiveperc:
-            topfive +=1
-
-        total+=1
-
-print("For layout baseline:")
-total_rmse = math.sqrt(total_se/i)/(total_vector_lengths/i)
-print(str(correct/total) + " of the predictions were exactly correct")
-print(str(toppointzeroone/total) + " of the predictions were in the top 0.01%")
-print(str(toppointone/total) + " of the predictions were in the top 0.1%")
-print(str(topone/total) + " of the predictions were in the top 1%")
-print(str(topfive/total) + " of the predictions were in the top 5%")
-print("rmse error is: " + str(total_rmse/i), flush=True)
-
 
 bert_size = 768
 
@@ -222,6 +144,88 @@ print(str(toppointone/total) + " of the predictions were in the top 0.1%")
 print(str(topone/total) + " of the predictions were in the top 1%")
 print(str(topfive/total) + " of the predictions were in the top 5%")
 print("rmse error is: " + str(total_rmse/i), flush=True)
+
+
+
+
+with open(args.precomp + "layout_eval.json") as f:
+    layout_emb = json.load(f, encoding='utf-8')
+
+predictor = BaselinePredictor(len(layout_emb[0][0]))
+predictor.load_state_dict(torch.load(args.layout_predictor))
+
+correct = 0
+toppointzeroone = 0
+toppointone = 0
+topone = 0
+topfive = 0
+total = 0
+
+dataset = TesterBaselineDataset(layout_emb, args.num_predictors)       
+data_loader = DataLoader(dataset, batch_size=1)
+
+data_itr = tqdm.tqdm(enumerate(data_loader),
+                              total=len(data_loader),
+                              bar_format="{l_bar}{r_bar}")
+
+i = 0
+total_se = 0
+total_vector_lengths = 0
+for idx, data in data_itr:
+    trace, index = data
+    trace = trace.squeeze(0)
+    index = index.squeeze(0)
+    result = predictor(trace)
+    i+=len(index)
+    # forward the training stuff (prediction)
+    # find which vocab vector has the smallest cosine distance
+    for idx in range(len(index)):
+        target = index[idx]
+        diff = result[idx].detach()-torch.tensor(dataset.embeddings[target])
+        sqer = sum(diff**2)
+        total_se += sqer
+        total_vector_lengths += np.linalg.norm(diff)
+        distances = scipy.spatial.distance.cdist(result[idx].unsqueeze(0).detach(), dataset.embeddings, "cosine")[0]
+
+        temp = np.argpartition(distances, (0, int(0.0001 * len(distances)), int(0.001 * len(distances)), int(0.01 * len(distances)), int(0.05 * len(distances)), int(0.1 * len(distances))))
+        closest_idx = temp[0]
+        closest_pointzerooneperc = temp[:int(0.0001 * len(distances))]
+        closest_pointoneperc = temp[:int(0.001 * len(distances))]
+        closest_oneperc = temp[:int(0.01 * len(distances))]
+        closest_fiveperc = temp[:int(0.05 * len(distances))]
+
+        if target==closest_idx:
+            correct +=1
+            toppointzeroone+=1
+            toppointone +=1
+            topone +=1
+            topfive +=1
+        elif target in closest_pointzerooneperc:
+            toppointzeroone +=1
+            toppointone +=1
+            topone +=1
+            topfive +=1
+        elif target in closest_pointoneperc:
+            toppointone +=1
+            topone +=1
+            topfive +=1
+        elif target in closest_oneperc:
+            topone +=1
+            topfive +=1
+        elif target in closest_fiveperc:
+            topfive +=1
+
+        total+=1
+
+print("For layout baseline:")
+total_rmse = math.sqrt(total_se/i)/(total_vector_lengths/i)
+print(str(correct/total) + " of the predictions were exactly correct")
+print(str(toppointzeroone/total) + " of the predictions were in the top 0.01%")
+print(str(toppointone/total) + " of the predictions were in the top 0.1%")
+print(str(topone/total) + " of the predictions were in the top 1%")
+print(str(topfive/total) + " of the predictions were in the top 5%")
+print("rmse error is: " + str(total_rmse/i), flush=True)
+
 
 
 # with open(args.precomp + "visual_eval.json") as f:
