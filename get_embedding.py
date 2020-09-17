1ac491da58b1ffcbaa0db8e9b0fd110efc4f8467
lipopowski <lpopowski@hmc.edu>
2020-09-17 09:17:09 -0700

    fixed single screen embedding

--- get_embedding.py
+++ get_embedding.py
@@ -8,7 +8,7 @@ from prediction import TracePredictor
 from autoencoder import ScreenLayout, LayoutAutoEncoder
 from UI_embedding.UI2Vec import HiddenLabelPredictorModel, UI2Vec
 from dataset.playstore_scraper import get_app_description
-from dataset.rico_utils import get_all_texts_from_rico_screen, get_all_labeled_texts_from_rico_screen, ScreenInfo
+from dataset.rico_utils import get_all_labeled_uis_from_rico_screen, ScreenInfo
 from dataset.rico_dao import load_rico_screen_dict
 
 
@@ -27,30 +27,29 @@ args = parser.parse_args()
 def get_embedding(screen, ui_model, screen_model, layout_model, num_predictors, net_version):
     with open(screen) as f:
         rico_screen = load_rico_screen_dict(json.load(f))
-    labeled_text = get_all_labeled_texts_from_rico_screen(rico_screen)
+    labeled_text = get_all_labeled_uis_from_rico_screen(rico_screen)
 
     bert = SentenceTransformer('bert-base-nli-mean-tokens')
     bert_size = 768
 
-    loaded_ui_model = HiddenLabelPredictorModel(bert, bert_size, 4)
+    loaded_ui_model = HiddenLabelPredictorModel(bert, bert_size, 16)
     loaded_ui_model.load_state_dict(torch.load(ui_model))
 
     ui_class = torch.tensor([UI[1] for UI in labeled_text])
     ui_text = [UI[0] for UI in labeled_text]
     UI_embeddings = loaded_ui_model.model([ui_text, ui_class])
 
-    avg_embedding = UI_embeddings.sum(dim=0)/len(labeled_text)
+    #avg_embedding = UI_embeddings.sum(dim=0)/len(labeled_text)
 
     try:
         package_name = rico_screen.activity_name.split("/")[0]
         descr = get_app_description(package_name)
     except Exception as e:
         descr = ''
-        print("hiya")
         print(str(e))
 
     descr_emb = torch.as_tensor(bert.encode([descr]), dtype=torch.float)
-
+    
     layout_autoencoder = LayoutAutoEncoder()
     layout_autoencoder.load_state_dict(torch.load(layout_model))
     layout_embedder = layout_autoencoder.enc
@@ -75,22 +74,26 @@ def get_embedding(screen, ui_model, screen_model, layout_model, num_predictors, 
         # no description in training case
         desc_size = 0
 
-    screen_embedder = Screen2Vec(bert_size, adus, adss, desc_size)
+    screen_embedder = Screen2Vec(bert_size, adus, adss, net_version)
     loaded_screen_model = TracePredictor(screen_embedder, net_version)
     loaded_screen_model.load_state_dict(torch.load(screen_model))
 
 
     if net_version in [1,3,4,5]:
         coords = torch.FloatTensor([labeled_text[x][2] for x in range(len(UI_embeddings))])
-        UI_embeddings = torch.cat((UI_embeddings,coords),dim=1)
+        UI_embeddings = torch.cat((UI_embeddings.cpu(),coords),dim=1)
     if net_version in [0,1,6]:
         screen_layout = None
-    else: screen_layout = encoded_layout
+    else: screen_layout = encoded_layout.unsqueeze(0).unsqueeze(0)
 
-    screen_emb = screen_embedder(UI_embeddings.unsqueeze(1).unsqueeze(0), descr_emb.unsqueeze(0), None, screen_layout.unsqueeze(0).unsqueeze(0), False)
+    screen_emb = screen_embedder(UI_embeddings.unsqueeze(1).unsqueeze(0), descr_emb.unsqueeze(0), None, screen_layout, False)
 
+    if descr_emb.size()[0] == 1:
+        descr_emb = descr_emb.squeeze(0)
+    #baseline_emb = torch.cat((avg_embedding, descr_emb), dim=0)
 
-    return avg_embedding, encoded_layout, screen_emb[0][0]
+    return encoded_layout, screen_emb[0][0]
 
 embeddings = get_embedding(args.screen, args.ui_model, args.screen_model, args.layout_model, args.num_predictors, args.net_version)
+print(embeddings)
 
