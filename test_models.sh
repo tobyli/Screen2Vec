1d4b6de58ee3f23cd3a6c3e441242bd1215996ac
lipopowski <lpopowski@hmc.edu>
2020-10-04 14:34:03 -0700

    testing adjusted to different model tester

--- test_models.sh
+++ test_models.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-python modeltester_screen.py -m "output/final_4.ep120" -d "precomp/example" -v 4 -n 4
-python modeltester_screen.py -m "output/final_6.ep80" -d "precomp/example" -v 6 -n 4
-python modeltester_screen.py -m "output/final_7.ep140" -d "precomp/example" -v 7 -n 4
-python modeltester_screen.py -m "output/final_8.ep140" -d "precomp/example" -v 8 -n 4
+python modeltester_screen.py -m4 "output/final_4.ep120" -m6 "output/final_6.ep80" -m7 "output/final_7.ep140" -m8 "output/final_8.ep140" -m9 "output/final_8.ep140" -d "precomp/example" -v 8 -n 4
