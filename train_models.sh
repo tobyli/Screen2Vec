96ed76ee13754d14940b9ce2d0e51454f37f24b9
lipopowski <lpopowski@hmc.edu>
2020-10-26 18:52:49 -0700

    adding shell script to train models

---
+++ train_models.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+
+python main_preloaded.py -d "precomp/hier_" -e 200 -o "output/hier6_" -r 0.01 -s 128 -b 256 -v 6 -n 4
+python main_preloaded.py -d "precomp/hier_" -e 200 -o "output/hier6_" -r 0.01 -s 128 -b 256 -v 7 -n 4
+python main_preloaded.py -d "precomp/hier_" -e 200 -o "output/hier6_" -r 0.01 -s 128 -b 256 -v 8 -n 4
