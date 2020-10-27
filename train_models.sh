b844233aa80c4e608e1f6dac1baa895d043fcdba
lipopowski <lpopowski@hmc.edu>
2020-10-27 16:14:45 -0700

    change model names in shell script

--- train_models.sh
+++ train_models.sh
@@ -4,3 +4,4 @@
 
 python main_preloaded.py -d "precomp/hier_" -e 200 -o "output/hier6_" -r 0.01 -s 128 -b 256 -v 6 -n 4
 python main_preloaded.py -d "precomp/hier_" -e 200 -o "output/hier7_" -r 0.01 -s 128 -b 256 -v 7 -n 4
+python main_preloaded.py -d "precomp/hier_" -e 200 -o "output/hier8_" -r 0.01 -s 128 -b 256 -v 8 -n 4
