c277e620018c342dcae1dd00aad7cea3a67ef90e
lipopowski <lpopowski@hmc.edu>
2020-12-28 17:04:25 -0800

    removing unnecessary shell scripts

--- test_models.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python modeltester_screen.py -m4 "output/hier_.ep200" -m6 "output/hier6_.ep100" -m7 "output/hier7_.ep200" -m8 "output/hier8_.ep120" -m9 "output/hier_.ep200" -d "precomp/hier_" -n 4
--- train_predictor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python write_baseline_models_for_prediction.py -d "/home/toby/Screen2Vec/datasets/filtered_traces" -o "output/baseline" -l "output/autoencoder.ep800"
-python for_baselines.py -d "output/baselinetext_eval.json" -o "output/text_predictor" -b 256 -e 300 -n 4 -r 0.0005
