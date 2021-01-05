d0e2d7ee61387982aa597bb49e9790eef4ec3cf4
lipopowski <lpopowski@hmc.edu>
2021-01-05 08:43:25 -0800

    adding shell script to download data

---
+++ download_data.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget https://storage.googleapis.com/crowdstf-rico-uiuc-4540/rico_dataset_v0.1/traces.tar.gz
+tar -xf traces.tar.gz
+
+wget https://storage.googleapis.com/crowdstf-rico-uiuc-4540/rico_dataset_v0.1/unique_uis.tar.gz
+tar -xf unique_uis.tar.gz
