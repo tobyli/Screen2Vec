f56b599086c8915ed66999e4a870d89aeca9de41
RachelJKim <racheljk02@gmail.com>
2025-02-03 06:23:27 +0000

    Update play scraper

--- UI_embedding/dataset/playstore_scraper.py
+++ UI_embedding/dataset/playstore_scraper.py
@@ -1,4 +1,4 @@
-import play_scraper
+import google_play_scraper 
 from os import path
 DEFAULT_APP_DESCRIPTION_PATH = './PlayStoreDescriptions/'
 
@@ -6,14 +6,8 @@ DEFAULT_APP_DESCRIPTION_PATH = './PlayStoreDescriptions/'
 
 def get_app_description(packageName):
     try:
-        return play_scraper.details(packageName)['description']
-    except ValueError as ve:
-        try:
-            potential_app = play_scraper.search(packageName, detailed=True)
-            return potential_app['description']
-        except Exception as e:
-            return ''
-    except Exception as e:
+        return google_play_scraper.app(packageName)['description']
+    except:
         return ''
 
 # scrape the playstore description for packageName and store it in path as a txt file
@@ -24,7 +18,7 @@ def update_app_description_file(packageName, dir_path):
         if (packageName + '\n') in unrecognizedPackageNames:
             return None
     try:
-        description = play_scraper.details(packageName)['description']
+        description = google_play_scraper.app(packageName)['description']
     except ValueError as e:
         if '404 Client Error: Not Found for url' in str(e):
             with open(dir_path + "unrecognized.text", "a") as f:
